a60e5538ac77d7914cdbed155d92dcea60edc88e
hyesoo <010-5225@naver.com>
2024-04-20 16:41:54 +0900

    login

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -2,47 +2,27 @@
     <div>
         <div class="pt-[110px] flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 gap-10">
             <div class="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 relative gap-5">
-            <div
-                class="flex flex-col justify-center items-start flex-grow-0 flex-shrink-0 w-[430px] relative gap-5"
-            >
+            <div class="flex flex-col justify-center items-start flex-grow-0 flex-shrink-0 w-[430px] relative gap-5">
                 <p class="flex-grow-0 flex-shrink-0 text-3xl font-bold text-left text-[#191919]">
                 로고 영역
                 </p>
                 <p class="flex-grow-0 flex-shrink-0 text-xl font-medium text-left text-[#191919]">
-                <span class="flex-grow-0 flex-shrink-0 text-xl font-medium text-left text-[#191919]"
-                    >클라이언트와 </span
-                ><br /><span
-                    class="flex-grow-0 flex-shrink-0 text-xl font-medium text-left text-[#191919]"
-                    >QA 파트너스의 만남</span
-                >
+                <span class="flex-grow-0 flex-shrink-0 text-xl font-medium text-left text-[#191919]">클라이언트와 </span><br/>
+                    <span class="flex-grow-0 flex-shrink-0 text-xl font-medium text-left text-[#191919]">QA 파트너스의 만남</span>
                 </p>
             </div>
-            <div class="flex-grow-0 flex-shrink-0 w-[430px] h-px bg-[#191919]"></div>
+                <div class="flex-grow-0 flex-shrink-0 w-[430px] h-px bg-[#191919]"></div>
             </div>
             <div class="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 gap-5">
-            <div
-                class="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[430px] h-[51px] relative overflow-hidden gap-12 p-4 rounded bg-white border border-[#ddd]"
-            >
-                <p class="flex-grow-0 flex-shrink-0 text-base text-left text-[#999]">이메일</p>
-            </div>
-            <div
-                class="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[430px] h-[51px] relative overflow-hidden gap-12 p-4 rounded bg-white border border-[#ddd]"
-            >
-                <p class="flex-grow-0 flex-shrink-0 text-base text-left text-[#999]">비밀번호</p>
-            </div>
+                <input type="text" v-model="params.email" class="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[430px] h-[51px] relative overflow-hidden gap-12 p-4 rounded bg-white border border-[#ddd]" @keydown="btnStatChng()" placeholder="이메일" />
+                <input type="password" v-model="params.pswd"  class="mb-[70px] flex justify-start items-center flex-grow-0 flex-shrink-0 w-[430px] h-[51px] relative overflow-hidden gap-12 p-4 rounded bg-white border border-[#ddd]" @keydown="btnStatChng()" placeholder="비밀번호" />
             </div>
         </div>
         <div class="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 gap-20">
-            <div
-            class="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[430px] relative overflow-hidden gap-2.5 px-2.5 py-4 rounded bg-[#999]"
-            >
-            <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white">
-                로그인 하기
-            </p>
-            </div>
+            <button class="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[430px] relative overflow-hidden gap-2.5 px-2.5 py-4 rounded bg-[#999] text-white" :class="params.btnIsActv ? 'bg-[#1BA494]': ''">로그인 하기</button>
             <div class="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 gap-10">
             <div class="flex justify-start items-center flex-grow-0 flex-shrink-0 relative gap-5">
-                <p class="flex-grow-0 flex-shrink-0 text-base text-left text-[#191919]">회원가입</p>
+                <button class="flex-grow-0 flex-shrink-0 text-base text-left text-[#191919]">회원가입</button>
                 <svg
                 width="1"
                 height="13"
@@ -54,7 +34,7 @@
                 >
                 <path d="M0.5 0.5V12.5" stroke="#DDDDDD" stroke-linecap="round"></path>
                 </svg>
-                <p class="flex-grow-0 flex-shrink-0 text-base text-left text-[#191919]">이메일 찾기</p>
+                <button class="flex-grow-0 flex-shrink-0 text-base text-left text-[#191919]">이메일 찾기</button>
                 <svg
                 width="1"
                 height="13"
@@ -66,10 +46,10 @@
                 >
                 <path d="M0.5 0.5V12.5" stroke="#DDDDDD" stroke-linecap="round"></path>
                 </svg>
-                <p class="flex-grow-0 flex-shrink-0 text-base text-left text-[#191919]">비밀번호 찾기</p>
+                <button class="flex-grow-0 flex-shrink-0 text-base text-left text-[#191919]">비밀번호 찾기</button>
             </div>
             <div class="flex justify-start items-center flex-grow-0 flex-shrink-0 relative gap-2.5">
-                <p class="flex-grow-0 flex-shrink-0 text-xs font-medium text-left text-[#777]">이용약관</p>
+                <button class="flex-grow-0 flex-shrink-0 text-xs font-medium text-left text-[#777]">이용약관</button>
                 <svg
                 width="1"
                 height="14"
@@ -81,9 +61,9 @@
                 >
                 <path d="M0.5 1V13" stroke="#DDDDDD" stroke-linecap="round"></path>
                 </svg>
-                <p class="flex-grow-0 flex-shrink-0 text-xs font-medium text-left text-[#777]">
+                <button class="flex-grow-0 flex-shrink-0 text-xs font-medium text-left text-[#777]">
                 개인정보처리방침
-                </p>
+                </button>
                 <svg
                 width="1"
                 height="14"
@@ -117,3 +97,27 @@
         </div>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return{
+            params:{
+                email:''
+               ,pswd:''
+               ,btnIsActv : false
+            }
+        }
+    }
+    ,methods:{
+        btnStatChng(){
+            if(this.params.email != '' && this.params.pswd != ''){
+                this.params.btnIsActv = true;
+            }else{
+                this.params.btnIsActv = false;
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
