3e20505f99a762f9531596d82d17d3723a71f455
sangpil <mr.sangpil@gmail.com>
2024-05-06 16:10:18 +0900

    pagination 추가

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import "./index.css";
 import router from "./routes/index.js";
 import VueCookies from "vue-cookies";
 import { createPinia } from 'pinia'
+// import the package
+import VueAwesomePaginate from "vue-awesome-paginate";
+// import the necessary css file
+import "vue-awesome-paginate/dist/style.css";
 //import baseMixin from "@/utils/baseMixin.js";
 //.mixin(baseMixin)
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(VueCookies).use(pinia).mount("#app");
+createApp(App).use(router).use(VueCookies).use(pinia).user(VueAwesomePaginate).mount("#app");
 
